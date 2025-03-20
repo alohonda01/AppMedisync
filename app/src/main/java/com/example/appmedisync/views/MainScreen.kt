6fc9297eb1e2687c7b859462c853b3d3b49a8d9b
alo <alondramtz.dev@icloud.com>
2025-03-20 13:47:47 -0600

    integracion de main screen prueba

---
+++ app/src/main/java/com/example/appmedisync/views/MainScreen.kt
@@ -0,0 +1,22 @@
+package com.example.appmedisync.views
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun mainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "¡Ha ingresado exitosamente!")
+    }
+}
